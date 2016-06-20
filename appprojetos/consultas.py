518409a73b9fe07f4daec4b821df213d9167ef90
ed9trabalho <ed9trabalho@gmail.com>
2016-06-20 16:22:26 -0300

    sas

--- appprojetos/consultas.py
+++ appprojetos/consultas.py
@@ -1,17 +1,4 @@
 from appprojetos.models import *
-'''''
-for e in ProjetoPesquisa.objects.all():
-    print(e.titulo,end=' membros do projeto \n')
-    for i in e.membro.all():
-        print(i.nome)
-
-for at in Atividade.objects.filter(dataInicio__month=5,
-                                   dataInicio__year=2015):
-    print(at)
-
-for me in MembroParticipante.objects.filter(nome__startswith='A'):
-    print(me)
-'''''
 
 
 
