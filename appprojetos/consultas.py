5137c5b43430e4bdadbef7921f5a64b633a67f56
ed9trabalho <ed9trabalho@gmail.com>
2016-06-22 18:57:24 -0300

    Merge branch 'master' of https://github.com/ed9trabalho/gestaoprojetos
    Conflicts:
    	.idea/vcs.xml
    	appprojetos/consultas.py
    	appprojetos/populabd.py

--- appprojetos/consultas.py
+++ appprojetos/consultas.py
@@ -7,13 +7,6 @@ for e in ProjetoPesquisa.objects.all():
         print(i.nome)
 print("--------------------------- 4.a)")
 
-for at in Atividade.objects.filter(dataInicio__month=5,
-                                   dataInicio__year=2015):
-    print(at)
-print("---------------------------- 4.b)")
-
-for me in MembroParticipante.objects.filter(nome__startswith='A'):
-    print(me)
 
 print("------------------------------ 4.c)")
 ativ= ProjetoPesquisa.objects.all()
